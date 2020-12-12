8883e5b56117e1161599955303ae9abd56d00866
Vladislav0101 <by.vlad01@mail.ru>
2020-12-12 12:58:46 +0300

    ready arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -582,8 +582,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((res, item) => res[item], arr);
 }
 
 
@@ -605,8 +605,10 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middle = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) return arr.slice(middle, arr.length).concat(arr.slice(0, middle));
+  return arr.slice(middle + 1, arr.length).concat(arr[middle], arr.slice(0, middle));
 }
 
 
